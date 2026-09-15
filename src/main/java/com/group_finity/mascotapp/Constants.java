c4be724302532299b381da948f757b2bc1a4056d
Ruixing Wang <ruixingw@icloud.com>
2026-09-16 05:53:22 +0800

    chore: bump version to 2.2.1

--- src/main/java/com/group_finity/mascotapp/Constants.java
+++ src/main/java/com/group_finity/mascotapp/Constants.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 
 public class Constants {
 
-    public static final String APP_VERSION = "2.2.0";
+    public static final String APP_VERSION = "2.2.1";
 
     public static final Path JAR_DIR;
 
